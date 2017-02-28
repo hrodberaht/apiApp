4699f3b42745c87e350b10b0c913274ee8522ce5
Robert <seedraev@gmail.com>
2017-02-28 18:21:11 +0000

    add EJS templating Angular for admin

--- server.js
+++ server.js
@@ -3,17 +3,29 @@ var app = express();
 var port = process.env.PORT;
 var bodyParser = require("body-parser");
 var morgan = require("morgan");
+var path = require("path");
 
 
 var api = require("./api/routes/apiRoute");
+var admin = require("./admin/routes/adminRoute");
+var client = require("./client/routes/clientRoute");
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
+// view engine setup
+app.set('views', path.join(__dirname, 'client/views'));
+app.set('view engine', 'ejs');
+// static file
+app.use('/public',express.static(path.join(__dirname, '/public')));
+
 
 //routes
+app.use("/", client);
 app.use("/api", api);
+app.use("/admin", admin);
 
 
 
