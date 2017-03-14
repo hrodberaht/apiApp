1621f4dff39a8e9ee36313f5921552eaf977f48b
Robert <seedraev@gmail.com>
2017-03-14 18:28:06 +0000

    add simple test

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var chai = require("chai");
+
+var assert = chai.assert;
+
+describe('add', function() {
+  it('simple test is array empty', function() {
+    var arr = [];
+   
+    
+    assert.equal(arr.length, 0);
+  });
+});
